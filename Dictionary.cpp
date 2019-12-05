76098b10f3fbfb7f91f609f635008f7861c85de3
Ayane Satomi <sr229@coder.com>
2019-12-05 20:58:13 +0800

    dictionary: Add basic init function and checker for adj

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,8 +1,39 @@
 #include "Dictionary.h"
 
+void Dictionary::init()
+{
+	std::ifstream adjDictFile("dictionary_adjectives.txt");
+	std::ifstream verbsDictFile("dictionary_verbs.txt");
+	std::ifstream nounsDictFile("dictionary_nouns.txt");
+
+	if (adjDictFile.good() && verbsDictFile.good() && nounsDictFile.good())
+	{
+		
+		std::string currentAdJStream;
+		std::string currentNounStream;
+		std::string currentVerbStream;
+
+		// stream it to the vectors
+		// hopefully it doesn't cause a big ass leak >_<
+		while (adjDictFile >> currentAdJStream && verbsDictFile >> currentVerbStream && nounsDictFile >> currentNounStream)
+		{
+			Dictionary::nouns.push_back(currentNounStream);
+			Dictionary::adjectives.push_back(currentAdJStream);
+			Dictionary::verbs.push_back(currentVerbStream);
+		}
+	}
+}
+
 bool Dictionary::isTokenAdj(std::string token)
 {
-	//TODO
+	if (std::find(Dictionary::adjectives.begin(), Dictionary::adjectives.end(), token) != Dictionary::adjectives.end())
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
 
 bool Dictionary::isTokenNoun(std::string token)
