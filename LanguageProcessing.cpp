eeda29e4b8284dfcf1186e495c2b81003cb1bf66
Ayane Satomi <sr229@coder.com>
2019-12-12 10:20:22 +0800

    main: complete main program

--- LanguageProcessing.cpp
+++ LanguageProcessing.cpp
@@ -40,11 +40,53 @@ int main()
 
         if (rawSentence != "e")
         {
-            // TODO: put token processing here.
+            // process token
             auto tokens = tp.getTokens(rawSentence);
+            //...
+
+            std::cout << "Sentence Analysis: " << std::endl;
+            std::cout << "=================================" << std::endl;
+            std::cout << "Word\t\tClassification" << std::endl;
+            std::cout << "=================================" << std::endl;
+
+            for (const auto& token : tokens)
+            {
+                if (dict.isTokenAdj(token))
+                {
+                    std::cout << token << "\t\t" << "adjective" << std::endl;
+                }
+                else if (dict.isTokenNoun(token))
+                {
+                    std::cout << token << "\t\t" << "noun" << std::endl;
+                }
+                else if (dict.isTokenConjunction(token))
+                {
+                    std::cout << token << "\t\t" << "conjunction" << std::endl;
+                }
+                else if (dict.isTokenPronoun(token))
+                {
+                    std::cout << token << "\t\t" << "pronoun" << std::endl;
+                }
+                else if (dict.isTokenVerb(token))
+                {
+                    std::cout << token << "\t\t" << "verb" << std::endl;
+                }
+                else if (dict.isTokenPreposition(token))
+                {
+                    std::cout << token << "\t\t" << "preposition" << std::endl;
+                }
+                else
+                {
+                    std::cout << token << "\t\t" << "UNKNOWN" << std::endl;
+                }
+            }
+            std::cout << "--------------------------------------" << std::endl;
+            std::cout << std::endl;
+
         }
         else
         {
+            // break the loop
             break;
         }
     }
